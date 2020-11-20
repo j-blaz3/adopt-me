62dfbfb059db53bdfb8c8ffaec54738ab324efc4
j-blalock <justin.m.blalock@gmail.com>
2020-11-20 17:22:04 -0600

    reusable components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+const Pet = () => {
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, "Remy"),
+		React.createElement("h2", {}, "Dog"),
+		React.createElement("h2", {}, "Lab")
+	]);
+};
+
+const App = () => {
+	return React.createElement("div", { id: "something-important" }, [
+			React.createElement("h1", {}, "Adopt Me!"),
+			React.createElement(Pet),
+			React.createElement(Pet),
+			React.createElement(Pet)
+		]);
+};
+
+ReactDOM.render(React.createElement(App), 
+document.getElementById("root")
+);
